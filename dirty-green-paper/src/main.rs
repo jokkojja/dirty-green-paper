9fc0a4a889d279be6be1c079f8e18f9ad4fc336c
jokkojja <102030405060708091k@gmail.com>
2024-06-10 18:17:10 +0700

    axum hello world

--- dirty-green-paper/src/main.rs
+++ dirty-green-paper/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, serve, Router};
+
+use tokio::{main, net::TcpListener};
+
+#[main]
+async fn main() {
+    let app: Router<_> = Router::new().route("/", get(|| async { "Hello world" }));
+
+    let listener: TcpListener = TcpListener::bind("0.0.0.0:8000").await.unwrap();
+
+    serve(listener, app).await.unwrap();
 }
