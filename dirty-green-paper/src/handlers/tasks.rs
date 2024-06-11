e0b5277b015d5343aaa50cb8545ca1abdb35426a
jokkojja <102030405060708091k@gmail.com>
2024-06-11 12:49:23 +0700

    Practicing

---
+++ dirty-green-paper/src/handlers/tasks.rs
@@ -0,0 +1,9 @@
+enum Currency {
+    Rub,
+    Usd,
+}
+
+struct Income {
+    amount: f32,
+    currency: Currency,
+}
--- dirty-green-paper/src/main.rs
+++ dirty-green-paper/src/main.rs
@@ -2,11 +2,54 @@ use axum::{routing::get, serve, Router};
 
 use tokio::{main, net::TcpListener};
 
+use std::env;
+
+use dotenv::dotenv;
+
+struct ApiConfig {
+    host: String,
+    port: String,
+}
+
+impl ApiConfig {
+    fn from_env() -> Self {
+        let host: String = env::var("HOST").unwrap();
+        let port: String = env::var("PORT").unwrap();
+
+        return ApiConfig {
+            host: host,
+            port: port,
+        };
+    }
+}
+
+struct Api {
+    listener: TcpListener,
+    router: Router,
+}
+
+impl Api {
+    async fn from_env() -> Self {
+        let config: ApiConfig = ApiConfig::from_env();
+
+        let router: Router = Router::new()
+            .route("/router1/hello", get(|| async { "Hello world router 1" }))
+            .route("/router2/hello", get(|| async { "Hello world router 2" }))
+            .route("/router3/hello", get(|| async { "Hello world router 3" }));
+
+        let addr: String = config.host + ":" + &config.port;
+
+        let listener: TcpListener = TcpListener::bind(addr).await.unwrap();
+
+        Api { listener, router }
+    }
+}
+
 #[main]
 async fn main() {
-    let app: Router<_> = Router::new().route("/", get(|| async { "Hello world" }));
+    dotenv().ok();
 
-    let listener: TcpListener = TcpListener::bind("0.0.0.0:8000").await.unwrap();
+    let api: Api = Api::from_env().await;
 
-    serve(listener, app).await.unwrap();
+    serve(api.listener, api.router).await.unwrap();
 }
